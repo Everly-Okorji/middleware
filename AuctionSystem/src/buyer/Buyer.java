095f2b2e45caf94cffb46a9eeece836d7d588235
Everly <okorji.2@utexas.edu>
2013-10-15 12:15:26 -0500

    Created interfaces for buyer, seller and broker

---
+++ AuctionSystem/src/buyer/Buyer.java
@@ -0,0 +1,15 @@
+package buyer;
+
+public interface Buyer {
+
+	void bid();
+	
+	void interest();
+	
+	void receiveBid();
+	
+	void interestBidUpdate();
+	
+	void itemSold();
+	
+}
---
+++ AuctionSystem/src/buyer/GeneralBuyer.java
@@ -0,0 +1,39 @@
+package buyer;
+
+public class GeneralBuyer implements Buyer {
+
+	public GeneralBuyer() {
+		
+	}
+	
+	@Override
+	public void bid() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void interest() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void receiveBid() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void interestBidUpdate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void itemSold() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
